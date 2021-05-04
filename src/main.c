722685f91f644f98667b85c47ea2d43588244df2
panda-roux <contact@panda-roux.dev>
2021-05-04 22:06:47 +0000

    added default document handling for directory URLs

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 #include "util.h"
 
 #define PORT 1965
-#define DEFAULT_DOCUMENT "/index.gmi"
+#define DEFAULT_DOCUMENT "index.gmi"
 #define EXT_GMI ".gmi"
 
 static bool path_is_gmi(const char* path) {
@@ -21,14 +21,48 @@ static bool path_is_gmi(const char* path) {
   return strcmp(strrchr(path, '.'), EXT_GMI) == 0;
 }
 
+static bool is_dir(const char* path) { return strrchr(path, '.') == NULL; }
+
+static char* append_default_doc(const request_t* request) {
+  size_t path_buf_len =
+      (request->path_length) + sizeof(DEFAULT_DOCUMENT) / sizeof(char);
+  char* path = malloc((path_buf_len + 1) * sizeof(char));
+  if (path == NULL) {
+    LOG_ERROR("Failed to append default document name to URL");
+    return NULL;
+  }
+
+  memcpy(path, request->path, request->path_length * sizeof(char));
+  if (request->path[request->path_length - 1] != '/') {
+    path[request->path_length] = '/';
+    memcpy(&path[request->path_length + 1], &DEFAULT_DOCUMENT[0],
+           sizeof(DEFAULT_DOCUMENT));
+  } else {
+    memcpy(&path[request->path_length], &DEFAULT_DOCUMENT[0],
+           sizeof(DEFAULT_DOCUMENT));
+  }
+
+  path[path_buf_len] = '\0';
+
+  return path;
+}
+
 static callback_result_t handle_request(const request_t* request,
                                         response_t* response) {
-  const char* path =
-      request->path_length > 1 ? request->path : DEFAULT_DOCUMENT;
+  char* path;
+  if (is_dir(request->path)) {
+    path = append_default_doc(request);
+  } else {
+    path = strndup(request->path, request->path_length);
+  }
+
+  LOG("Path: %s", path);
+
   FILE* file = fopen(path + 1, "rb");
   if (file == NULL) {
     response->status = STATUS_NOT_FOUND;
     response->meta = strdup("File does not exist");
+    free(path);
     return ERROR;
   }
 
@@ -44,8 +78,6 @@ static callback_result_t handle_request(const request_t* request,
     //
     // not using default path and the path isn't .gmi; so we need to infer
     // mimetype
-
-    LOG("path: %s", path);
 
     response->mimetype = get_mimetype(path);
     response->status = STATUS_SUCCESS;
@@ -65,6 +97,8 @@ static callback_result_t handle_request(const request_t* request,
       result = OK;
     }
   }
+
+  free(path);
 
   fclose(file);
 
