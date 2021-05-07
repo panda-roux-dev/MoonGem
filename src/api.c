66b1095e9fe3bdee593d5aa393cdadfa09e33cbc
panda-roux <contact@panda-roux.dev>
2021-05-07 00:41:50 +0000

    simplifying Lua API calls
    Signed-off-by: panda-roux <contact@panda-roux.dev>

--- src/api.c
+++ src/api.c
@@ -1,3 +1,4 @@
+#include <lauxlib.h>
 #include <lua.h>
 #include <stdio.h>
 
@@ -5,37 +6,12 @@
 #include "script.h"
 #include "util.h"
 
-#define ARG_COUNT(L, n)                                                        \
-  {                                                                            \
-    int argc = lua_gettop(L);                                                  \
-    if (argc < (n) + 1) {                                                      \
-      lua_pushfstring(L,                                                       \
-                      "Too few arguments passed to %s: expected %d, found %d", \
-                      __func__, n, argc - 1);                                  \
-      lua_error(L);                                                            \
-      return 0;                                                                \
-    }                                                                          \
-  }
-
-#define ARG_TYPE(L, n, type)                                                \
-  {                                                                         \
-    if (!lua_is##type(L, (n) + 1)) {                                        \
-      lua_pushfstring(                                                      \
-          L, "Argument %d passed to %s in an invalid type; expected %s", n, \
-          __func__, #type);                                                 \
-      lua_error(L);                                                         \
-      return 0;                                                             \
-    }                                                                       \
-  }
-
 #define SCRIPT_ERR(L, fmt, ...)             \
   {                                         \
     lua_pushfstring(L, fmt, ##__VA_ARGS__); \
     lua_error(L);                           \
   }
 
-#define ARG(n) ((n) + 1)
-#define SELF 1
 #define LINK_TOKEN "=>"
 #define HEADER_TOKEN "#"
 #define QUOTE_TOKEN ">"
@@ -46,11 +22,10 @@
 int api_head_set_lang(lua_State* L) { return 0; }
 
 int api_body_include(lua_State* L) {
-  ARG_COUNT(L, 1);
-  ARG_TYPE(L, 1, string);
-
-  const char* path = lua_tostring(L, ARG(1));
-  lua_pop(L, 1);
+  const char* path = luaL_checkstring(L, 2);
+  if (path == NULL) {
+    return 0;
+  }
 
   char* contents = NULL;
   size_t file_len = read_file(path, &contents);
@@ -60,173 +35,164 @@ int api_body_include(lua_State* L) {
   }
 
   if (file_len > 0) {
-    lua_getfield(L, SELF, TBL_RESPONSE);
+    lua_getfield(L, 1, TBL_RESPONSE);
     lua_getfield(L, -1, FLD_BUFFER);
-    lua_pushstring(L, contents);
-    lua_pushliteral(L, "\n");
-    lua_concat(L, 3);
 
-    lua_setfield(L, SELF + 1, FLD_BUFFER);
+    lua_pushfstring(L, "%s" NEWLINE, contents);
+
+    lua_concat(L, 2);
+    lua_setfield(L, -2, FLD_BUFFER);
   }
 
   return 0;
 }
 
 int api_body_write(lua_State* L) {
-  ARG_COUNT(L, 1);
-  ARG_TYPE(L, 1, string);
+  const char* text = luaL_checkstring(L, 2);
+  if (text == NULL) {
+    return 0;
+  }
 
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
-  lua_rotate(L, 2, -1);
+
+  lua_pushstring(L, text);
+
   lua_concat(L, 2);
-  lua_setfield(L, 2, FLD_BUFFER);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_line(lua_State* L) {
-  ARG_COUNT(L, 1);
-  ARG_TYPE(L, 1, string);
+  const char* text = luaL_checkstring(L, 2);
+  if (text == NULL) {
+    return 0;
+  }
 
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
-  lua_rotate(L, 2, -1);
-  lua_pushliteral(L, NEWLINE);
-  lua_concat(L, 3);
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+
+  lua_pushfstring(L, "%s" NEWLINE, text);
+
+  lua_concat(L, 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_link(lua_State* L) {
-  ARG_COUNT(L, 1);
-
-  if (lua_gettop(L) == 2) {
-    ARG_TYPE(L, 1, string);
-
-    // URL only, no alt-text
-
-    lua_pushliteral(L, SPACE);
-    lua_pushliteral(L, LINK_TOKEN);
-    lua_getfield(L, SELF, TBL_RESPONSE);
-    lua_getfield(L, -1, FLD_BUFFER);
-    lua_rotate(L, 2, 2);
-    lua_rotate(L, 4, 1);
-    lua_rotate(L, 5, 1);
-    lua_pushliteral(L, NEWLINE);
-    lua_concat(L, 5);
-  } else {
-    ARG_TYPE(L, 1, string);
-    ARG_TYPE(L, 2, string);
-
-    // URL + alt-text
-
-    lua_pushliteral(L, SPACE);
-    lua_pushliteral(L, LINK_TOKEN);
-    lua_pushliteral(L, SPACE);
-    lua_getfield(L, SELF, TBL_RESPONSE);
-    lua_getfield(L, -1, FLD_BUFFER);
-    lua_rotate(L, 2, 2);
-    lua_rotate(L, 4, 2);
-    lua_rotate(L, 7, 1);
-    lua_pushliteral(L, NEWLINE);
-    lua_concat(L, 7);
+  const char* url = luaL_checkstring(L, 2);
+  if (url == NULL) {
+    return 0;
   }
 
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+  lua_getfield(L, 1, TBL_RESPONSE);
+  lua_getfield(L, -1, FLD_BUFFER);
+
+  if (lua_gettop(L) == 2) {
+    // URL only, no alt-text
+
+    lua_pushfstring(L, LINK_TOKEN " %s" NEWLINE, url);
+  } else {
+    // URL + alt-text
+
+    const char* alt = luaL_checkstring(L, 3);
+    if (alt == NULL) {
+      return 0;
+    }
+
+    lua_pushfstring(L, LINK_TOKEN " %s %s" NEWLINE, url, alt);
+  }
+
+  lua_concat(L, 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_heading(lua_State* L) {
-  ARG_COUNT(L, 2);
-  ARG_TYPE(L, 1, string);
-  ARG_TYPE(L, 2, integer);
+  const char* text = luaL_checkstring(L, 2);
+  if (text == NULL) {
+    return 0;
+  }
 
-  int level = lua_tointeger(L, ARG(2));
-  lua_pop(L, 1);
+  int level = luaL_optinteger(L, 3, 1);
 
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
 
   for (int i = 0; i < level; ++i) {
     lua_pushliteral(L, "#");
   }
 
-  lua_pushliteral(L, SPACE);
-  lua_rotate(L, 2, -1);
-  lua_pushliteral(L, NEWLINE);
-  lua_concat(L, level + 4);
+  lua_pushfstring(L, " %s" NEWLINE, text);
 
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+  lua_concat(L, level + 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_quote(lua_State* L) {
-  ARG_COUNT(L, 1);
-  ARG_TYPE(L, 1, string);
+  const char* text = luaL_checkstring(L, 2);
+  if (text == NULL) {
+    return 0;
+  }
 
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
 
-  lua_pushliteral(L, QUOTE_TOKEN);
-  lua_rotate(L, 2, -1);
-  lua_pushliteral(L, NEWLINE);
-  lua_concat(L, 4);
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+  lua_pushfstring(L, QUOTE_TOKEN " %s" NEWLINE, text);
+
+  lua_concat(L, 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_block(lua_State* L) {
-  ARG_COUNT(L, 1);
-  ARG_TYPE(L, 1, string);
+  const char* text = luaL_checkstring(L, 2);
+  if (text == NULL) {
+    return 0;
+  }
 
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
-  lua_pushliteral(L, BLOCK_TOKEN);
-  lua_pushliteral(L, NEWLINE);
-  lua_pushliteral(L, BLOCK_TOKEN);
-  lua_pushliteral(L, NEWLINE);
-  lua_rotate(L, 2, -1);
-  lua_rotate(L, 5, -2);
-  lua_pushliteral(L, NEWLINE);
-  lua_concat(L, 7);
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+
+  lua_pushfstring(L, BLOCK_TOKEN NEWLINE "%s" NEWLINE BLOCK_TOKEN, text);
+
+  lua_concat(L, 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_beginblock(lua_State* L) {
-  lua_getfield(L, SELF, TBL_RESPONSE);
-  lua_getfield(L, -1, FLD_BUFFER);
-  lua_pushliteral(L, BLOCK_TOKEN);
+  const char* alt = luaL_checkstring(L, 2);
 
-  if (lua_gettop(L) >= 2) {
-    // optional first argument for alt-text
-    lua_rotate(L, 2, -1);
-    lua_pushliteral(L, NEWLINE);
-    lua_concat(L, 4);
+  lua_getfield(L, 1, TBL_RESPONSE);
+  lua_getfield(L, -1, FLD_BUFFER);
+
+  if (alt == NULL) {
+    lua_pushliteral(L, BLOCK_TOKEN NEWLINE);
   } else {
-    lua_pushliteral(L, NEWLINE);
-    lua_concat(L, 3);
+    lua_pushfstring(L, BLOCK_TOKEN "%s" NEWLINE, alt);
   }
 
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+  lua_concat(L, 2);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
 
 int api_body_endblock(lua_State* L) {
-  lua_getfield(L, SELF, TBL_RESPONSE);
+  lua_getfield(L, 1, TBL_RESPONSE);
   lua_getfield(L, -1, FLD_BUFFER);
-  lua_pushliteral(L, BLOCK_TOKEN);
-  lua_pushliteral(L, NEWLINE);
-  lua_concat(L, 3);
+  lua_pushliteral(L, BLOCK_TOKEN NEWLINE);
+  lua_concat(L, 2);
 
-  lua_setfield(L, SELF + 1, FLD_BUFFER);
+  lua_setfield(L, -2, FLD_BUFFER);
 
   return 0;
 }
